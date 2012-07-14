360e69d646c316321a2229ac6275fd21b8102112
Suresh <surysharma@gmail.com>
2012-07-14 12:57:12 +0100

    We cannot move spring-servlet.xml as this location is used by Dispatcher servlet at the boot time, reverted the change

--- src/test/java/com/hedgebenefits/daos/impl/AdminDaoIntegrationTest.java
+++ src/test/java/com/hedgebenefits/daos/impl/AdminDaoIntegrationTest.java
@@ -14,7 +14,7 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
  * To change this template use File | Settings | File Templates.
  */
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration("/daoAccess.xml")
+@ContextConfiguration("/daoContext.xml")
 public class AdminDaoIntegrationTest {
     @Before
     public void setUp() throws Exception {
