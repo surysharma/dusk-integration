abd41cf8aba8b819e253d7193da84210c9716e1a
Suresh <surysharma@gmail.com>
2012-09-25 19:49:19 +0100

    Hibernte config setup

---
+++ src/main/java/com/hedgebenefits/util/HibernateUtils.java
@@ -0,0 +1,15 @@
+package com.hedgebenefits.util;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.AnnotationConfiguration;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: sureshsharma
+ * Date: 25/09/2012
+ * Time: 19:35
+ * To change this template use File | Settings | File Templates.
+ */
+public class HibernateUtils {
+    public static SessionFactory sessionFactory = new AnnotationConfiguration().configure("").buildSessionFactory();
+}
