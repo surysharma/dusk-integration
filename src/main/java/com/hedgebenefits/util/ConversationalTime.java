9d7c6041f6aa34432df036d9d1ed2b5ee49a680c
Suresh <surysharma@gmail.com>
2012-12-10 19:49:38 +0000

    Utitlity to be added to Joda time

---
+++ src/main/java/com/hedgebenefits/util/ConversationalTime.java
@@ -0,0 +1,11 @@
+package com.hedgebenefits.util;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: sureshsharma
+ * Date: 24/11/2012
+ * Time: 07:46
+ * To change this template use File | Settings | File Templates.
+ */
+public class ConversationalTime {
+}
