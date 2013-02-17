d76e4be86093a9ae26a221193d6d4695cd6f388c
Suresh <surysharma@gmail.com>
2013-02-17 20:01:38 +0000

    Added GEneralization

--- interop/src/main/java/com/interop/REST/resources/AdminResource.java
+++ interop/src/main/java/com/interop/REST/resources/AdminResource.java
@@ -9,6 +9,8 @@ import javax.ws.rs.core.*;
 import javax.xml.bind.JAXBElement;
 import java.util.Map;
 
+import static java.lang.String.valueOf;
+
 /**
  * Created by IntelliJ IDEA.
  * User: sureshsharma
@@ -60,14 +62,21 @@ public class AdminResource {
     @PUT
     @Path("{id}")
     @Consumes({MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML})
-    public Response updateAdmin(@PathParam("id") Long id, JAXBElement<Admin> admin) {
+    public Response updateAdmin(@PathParam("id") Long id, JAXBElement<Admin> admin, @Context Request request) {
         Map<Long, Admin> adminData = adminRepository.getAll();
         Admin existingAdmin = adminData.get(id);
         if (existingAdmin == null) {
             return Response.ok().status(Response.Status.NOT_FOUND).build();
         }
+
+        EntityTag entityTag = new EntityTag(valueOf(admin.hashCode()));
+//        Response.ResponseBuilder responseBuilder = request.evaluatePreconditions(entityTag);
+//        if (responseBuilder != null){
+//            throw new WebApplicationException(responseBuilder.build());
+//        }
+
         adminData.put(id, admin.getValue());
-        return Response.ok().status(Response.Status.CREATED).build();
+        return Response.ok().status(Response.Status.CREATED).tag(entityTag).build();
     }
 
 
--- src/test/java/com/hedgebenefits/restclient/CrudAdminWithJerseyClient.java
+++ src/test/java/com/hedgebenefits/restclient/CrudAdminWithJerseyClient.java
@@ -11,6 +11,7 @@ import javax.ws.rs.core.MediaType;
 import java.util.HashMap;
 import java.util.Map;
 
+import static org.hamcrest.CoreMatchers.notNullValue;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.Is.is;
 
@@ -28,13 +29,13 @@ public class CrudAdminWithJerseyClient {
     private static String HTTP = "http";
     private static String HOSTNAME = "localhost";
     private static String PORT = "8080";
+    private static final String IF_MATCH = "If_Match";
 
 
     @Test
     public void shouldPerformConditionalGet() {
         //given a URI template
-        Map<String, String> pathParams = new HashMap<String, String>();
-        pathParams.put("id", "3");
+        Map<String, String> pathParams = newHashMap(new String[]{"id", "3"});
         String uri = UriTemplate.createURI(HTTP, "", HOSTNAME, PORT, "/rest/admin/id/{id}", "", "", pathParams, true);
         //and a web resource for it
         WebResource resource = client.resource(uri);
@@ -49,6 +50,8 @@ public class CrudAdminWithJerseyClient {
         assertThat(firstResponse.getClientResponseStatus().getStatusCode(), is(STATUS_OK));
         System.out.println(firstResponse.getClientResponseStatus().getStatusCode());
 
+        //*********************************************************//
+
         //when a second GET request is made using the same Entity tag value
         String value = firstResponse.getEntityTag().getValue();
         ClientResponse secondResponse = resource
@@ -58,8 +61,55 @@ public class CrudAdminWithJerseyClient {
 
         //then the Status code returned is
         assertThat(secondResponse.getClientResponseStatus().getStatusCode(), is(NOT_ALLOWED));
+        assertThat(secondResponse.getLength(), is(-1));
+        assertThat(secondResponse.getEntityTag().getValue(), is(value));
         System.out.println(secondResponse.getClientResponseStatus().getStatusCode());
     }
 
+    @Test
+    public void shouldPerformConditionalPut() {
+        //Given a GET request is made for a resource
+        Map<String, String> pathParams = newHashMap(new String[]{"id", "1"});
+        String getUri = UriTemplate.createURI(HTTP, "", HOSTNAME, PORT, "/rest/admin/id/{id}", "", "", pathParams, true);
+        //and a web resource for it
+        WebResource resource = client.resource(getUri);
+        System.out.println(resource.getURI());
+
+        //when GET request is made for 1st time
+        ClientResponse firstResponse = resource
+                .accept(MediaType.APPLICATION_JSON_TYPE)
+                .get(ClientResponse.class);
+
+
+        //And a URI template
+        Map<String, String> pathParamValues = newHashMap(new String[]{"id", "1"});
+        String putUri = UriTemplate
+                .createURI(HTTP, "", HOSTNAME, PORT, "/rest/admin/update/id/{id}", "", "", pathParamValues, true);
+
+
+        //when
+        ClientResponse updateResponse = client
+                .resource(putUri)
+                .accept(MediaType.APPLICATION_JSON_TYPE)
+                .header(IF_MATCH, "\""+ firstResponse.getEntityTag().getValue() + "\"")
+                .entity("{\"rights\":\"updatedrights1\",\"username\":\"updatedadmin1\"}", MediaType.APPLICATION_JSON_TYPE)
+                .put(ClientResponse.class);
+        System.out.println(putUri);
+
+        //then
+        assertThat(updateResponse.getClientResponseStatus().getStatusCode(), is(201));
+        assertThat(updateResponse.getEntityTag(), notNullValue());
+    }
+
+    private <T> HashMap<T, T> newHashMap(T[]... items) {
+        if (items == null){
+            throw new RuntimeException("Parameter cannot be null");
+        }
+        HashMap<T, T> map = new HashMap<T, T>();
+        for (T[] item : items) {
+            map.put(item[0], item[1]);
+        }
+        return map;
+    }
 
 }
